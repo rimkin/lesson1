1976f0a2434e3dfb5c42264e2816a976874fce92
rimkin <rimkin@mail.ru>
2019-11-11 13:30:29 +0200

    Working with file exc2

--- excersize2.py
+++ excersize2.py
@@ -1,26 +1,26 @@
 import sys
-
+import os
 def f1(a):
-    print 'hello function a=',a
+    print ('hello function a=',a)
 
 # comment1
 def f2(r):
     if type(r) != list:
-        print 'not a list',r
+        print ('not a list',r)
         return
 
     index=0
     for item in r:
-        print 'item[',index,']=',item
+        print ('item[',index,']=',item)
         index=index+1
 
     for i in range(0,len(r)):
         item=r[i]
-        print 'item[',i,']=',r[i]
+        print ('item[',i,']=',r[i])
         
 
-print sys.version
-print 'making functions'
+print (sys.version)
+print ('making functions')
 a=1
 b='1'
 c=[1,2,3]
@@ -30,7 +30,7 @@ full_list.append(a)
 full_list.append(b)
 full_list.append(c)
 full_list.append(d)
-print 'full_list=',full_list
+print ('full_list=',full_list)
 f2(a)
 f2(b)
 f2(c)
